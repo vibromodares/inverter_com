ee94736ec5f2e4ea3027f7990c97bf3378f7e428
siavash <sepahisiavash@gmail.com>
2024-06-24 07:33:44 +0330

    cleaning

--- MainCode.py
+++ MainCode.py
@@ -1,8 +1,10 @@
 import os
 import sys
+from datetime import time
+from time import sleep
+
 from PyQt5.QtWidgets import QApplication
 
-from app.inverter.iG5A.iG5A_model import iG5AModel
 
 def resource_path(relative_path):
     try:
@@ -17,8 +19,6 @@ path = resource_path("")
 path = path.replace(path[2], "/")
 
 if __name__ == "__main__":
-    # iG5A = iG5AModel()
-
     app = QApplication(sys.argv)
     from core.app_provider.admin.main import Main
 
@@ -34,6 +34,60 @@ if __name__ == "__main__":
     # print(serial.readSerial('01R03011A8'))
     # print(serial.readSerial('01W0380101F48F'))
     # print(serial.readSerial('01W03821000177'))
+
+    # import serial
+    # import binascii
+    # from time import sleep
+    #
+    #
+    # def initSerial(serial_in):
+    #     serial_in.baudrate = 9600
+    #     serial_in.port = 'COM12'
+    #     serial_in.stopbits = serial.STOPBITS_ONE
+    #     serial_in.bytesize = 8
+    #     serial_in.parity = serial.PARITY_NONE
+    #     serial_in.rtscts = 0
+    #     serial_in.timeout = 1
+    #
+    #
+    # def readSerial(data):
+    #     ser = serial.Serial()
+    #     initSerial(ser)
+    #     ser.open()
+    #
+    #     ENQ = chr(5)
+    #     EOT = chr(4)
+    #
+    #     byte = bytes(ENQ + data + EOT, 'utf-8')
+    #     ser.write(byte)
+    #
+    #     bstr = ''
+    #     sstr = ''
+    #     flag = False
+    #     while True:
+    #         mHex = ser.read()
+    #         if len(mHex) != 0:
+    #             flag = True
+    #             bstr += binascii.hexlify(bytearray(mHex)).decode("utf-8")
+    #             bstr += " "
+    #             sstr += mHex.decode("utf-8")
+    #
+    #         sleep(0.1)
+    #         if len(mHex) == 0 and flag:
+    #             break
+    #     return bstr, sstr
+    #
+    #
+    # if __name__ == "__main__":
+    #     print(readSerial('01R03001A7'))
+    #     print(readSerial('01R03011A8'))
+    #     print(readSerial('01W0380101F48F'))
+    #     print(readSerial('01W03821000177'))
+    #
+    #     sleep(5)
+    #     print(readSerial('01R030D1A8'))
+    #     sleep(10)
+    #     print(readSerial('01W03821000076'))
     #
     # sleep(2)
     # print(serial.readSerial('01R030D1A8'))
--- test.py
+++ test.py
@@ -1,30 +1,36 @@
-# db = DataBaseModel(file_name='inverter_models.json')
+# db = DataBaseModel(file_name='device_models.json')
 # db.insert_data({
-#     'model_id': '1',
+#     'model_id': 1,
 #     'name': 'iG5A',
 #     'com_disc': 'USB-SERIAL CH340',
 # })
 # db.insert_data({
-#     'model_id': '2',
+#     'model_id': 2,
 #     'name': 'PLC',
 #     'com_disc': 'USB-SERIAL CH340'
 # })
-from app.database.api.api import get_all_module
+from time import sleep
 
-# TODO:bayad check kone data on model inverter ra dare ya na dl kone v check kone bebine aslan in model inverter to model haye ma hast ya na
+from app.database.api.api import get_device_by_id
+from app.inverter.iG5A.iG5AModel_new import iG5AModel
+from app.inverter.inverter_model import InverterBaseModel
 
-
-# db = DataBaseModel(file_name='inverter_list.json')
+# from app.database.model.database_model import DataBaseModel
+#
+# # TODO:bayad check kone data on model inverter ra dare ya na dl kone v check kone bebine aslan in model inverter to model haye ma hast ya na
+#
+#
+# db = DataBaseModel(file_name='device_list.json')
 # db.insert_data({
-#     'inverter_id': '1',
-#     'model_id': '1',
+#     'inverter_id': 1,
+#     'model_id': 1,
 #     'name': 'main',
 #     'com_port': 'COM11',
 # })
 #
 # db.insert_data({
-#     'inverter_id': '2',
-#     'model_id': '1',
+#     'inverter_id': 2,
+#     'model_id': 1,
 #     'name': 'test',
 #     'com_port': 'COM12',
 # })
@@ -60,3 +66,66 @@ from app.database.api.api import get_all_module
 #
 # for port, desc, hwid in sorted(ports):
 #         print("{}: {} ".format(port, desc))
+
+
+# asd = InverterBaseModel()
+# asd.set_serial_com('COM10')
+# print(asd.test_com())
+# asd = InverterBaseModel()
+# asd.set_serial_com('COM10')
+# print(asd.start())
+
+
+# print(serial.readSerial('01R00001A4'))
+# print(time() - start_time)
+# start_time = time()
+# print(serial.readSerial('01R03001A7'))
+# print(time() - start_time)
+# print(serial.readSerial('01R03011A8'))
+# print(serial.readSerial('01W0380101F48F'))
+# print(serial.readSerial('01W03821000177'))
+
+# sleep(2)
+# print(serial.readSerial('01R030D1A8'))
+# sleep(2)
+# print(serial.readSerial('01W03821000076'))
+# import os
+# import sys
+# from datetime import time
+# from time import sleep
+#
+# from PyQt5.QtWidgets import QApplication
+#
+# app = QApplication(sys.argv)
+# from core.app_provider.admin.main import Main
+#
+# # main = Main()
+#
+# asd = get_device_by_id(1)
+# asd.start_com()
+# print('frq',asd.read_frq())
+# asd.set_frq_ui(4.82)
+#
+# print(asd.read_name())
+# print(asd.read_capacity())
+# print(asd.read_acc_time())
+# print(asd.read_deacc_time())
+# print('op',asd.read_operating_status())
+#
+# asd.set_acc_time_ui(250)
+# asd.set_deacc_time_ui(25)
+# asd.start()
+# sleep(2)
+# print('frq',asd.read_frq())
+# print('op',asd.read_operating_status())
+# sleep(20)
+# print('frq',asd.read_frq())
+# print('op',asd.read_operating_status())
+# asd.stop()
+# print('frq',asd.read_frq())
+# print('op',asd.read_operating_status())
+# sleep(5)
+# print('frq',asd.read_frq())
+# print('op',asd.read_operating_status())
+# sys.exit(app.exec_())
+# print(asd2)
